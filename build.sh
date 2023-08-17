f6ad54f7f8cf7faefc1a31a4b585785fec860684
Aryanxfa <aryan200152@gmail.com>
2023-08-17 12:12:21 +0530

    initial commit

---
+++ build.sh
@@ -0,0 +1,11 @@
+mkdir system
+mkdir vendor
+
+if [ "$#" -lt 1 ]; then
+    echo "No arguments supplied"
+    exit 1
+fi
+
+zip -r $1 META-INF vendor system
+adb push $1 /sdcard/
+adb shell twrp install /sdcard/$1
